fe6e6ae518b80efe2ad7f2a9ec2c2fb5dd1befc9
4pcbr <me@4pcbr.com>
2015-11-08 16:38:59 +0100

    fixed a mess with recursion args mess in murmur

--- murmur_hash.ex
+++ murmur_hash.ex
@@ -9,24 +9,30 @@ defmodule MurmurHash do
   @m  5
   @n  0xe6546b64
 
-  def _proc_block(<< b1, b2, b3, b4, tail :: binary >>, hash) do
+  def _proc_block(hash, << b1, b2, b3, b4, tail :: binary >>) do
     k = :binary.decode_unsigned(<<b1, b2, b3, b4>>, :big)
     k = k * @c1
     k = bor( ( k <<< @r1 ), ( k >>> ( 32 - @r1 ) ) )
     k = k * @c2
     hash = bxor( hash, k )
     hash = bor( ( hash <<< @r2 ), ( hash >>> ( 32 - @r2 ) ) ) * @m + @n
-    { hash, _proc_block(tail, hash) }
+    _proc_block(hash, tail)
   end
 
-  def _proc_block(<< tail :: binary >>, hash), do: { hash, tail }
+  def _proc_tail(hash, tail, k1) do
+    #TODO
+  end
+
+
+  def _proc_block(hash, << tail :: binary >>), do: { hash, tail }
 
   def murmur3_32(key, seed \\ 0x0000) do
     hash = seed
     len = byte_size key
     nblocks = div(len, 4)
-    { hash, tail } = _proc_block(key, hash)
-
+    { hash, tail } = _proc_block(hash, key)
+    k1 = 0x0000
+    { hash, tail, _ } = _proc_tail(hash, tail, k1)
   end
 
 end
