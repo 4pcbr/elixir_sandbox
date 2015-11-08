534794954cacb4ed4fd06a67fb60d64c5703b957
4pcbr <me@4pcbr.com>
2015-11-08 19:19:37 +0100

    The murmur code is complete but the result seems to be incorrect

--- murmur_hash.ex
+++ murmur_hash.ex
@@ -19,20 +19,41 @@ defmodule MurmurHash do
     _proc_block(hash, tail)
   end
 
-  def _proc_tail(hash, tail, k1) do
-    #TODO
+  def _proc_block(hash, << tail :: binary >>), do: { hash, tail }
+
+  def _proc_tail(hash, << b1, b2, b3 >>, k1) do
+    k1 = bxor( k1, b3 <<< 16 )
+    _proc_tail(hash, << b1, b2 >>, k1)
   end
 
+  def _proc_tail(hash, << b1, b2 >>, k1) do
+    k1 = bxor( k1, b2 <<< 8 )
+    _proc_tail(hash, << b1 >>, k1)
+  end
 
-  def _proc_block(hash, << tail :: binary >>), do: { hash, tail }
+  def _proc_tail(hash, << b1 >>, k1) do
+    k1 = bxor( k1, b1 )
+    k1 = k1 * @c1
+    k1 = bor( k1 <<< @r1, k1 >>> (32 - @r1) )
+    k1 = k1 * @c2
+    bxor( hash, k1 )
+  end
+
+  def _bxor_self_with_rshift(val, rshift), do: bxor( val, val >>> rshift )
+
+  def _mult_by(val, mult), do: val * mult
 
   def murmur3_32(key, seed \\ 0x0000) do
     hash = seed
     len = byte_size key
-    nblocks = div(len, 4)
     { hash, tail } = _proc_block(hash, key)
-    k1 = 0x0000
-    { hash, tail, _ } = _proc_tail(hash, tail, k1)
+    _proc_tail(hash, tail, 0x0000)
+      |> bxor len
+      |> _bxor_self_with_rshift 16
+      |> _mult_by 0x85ebca6b
+      |> _bxor_self_with_rshift 13
+      |> _mult_by 0xc2b2ae35
+      |> _bxor_self_with_rshift 16
   end
 
 end
