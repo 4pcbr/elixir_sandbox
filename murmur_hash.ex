05e2c0b5ab25241926ea104451f36b96718e55fa
4pcbr <me@4pcbr.com>
2015-11-09 10:06:17 +0100

    Implemented Murmurhash3_32

--- murmur_hash.ex
+++ murmur_hash.ex
@@ -1,59 +1,71 @@
-defmodule MurmurHash do
+defmodule MurmurHash.MurmurHash3_32 do
 
   use Bitwise
 
-  @c1 0xcc9e2d51
-  @c2 0x1b873593
-  @r1 15
-  @r2 13
-  @m  5
-  @n  0xe6546b64
+  @mask_32 0xFFFFFFFF
 
-  def _proc_block(hash, << b1, b2, b3, b4, tail :: binary >>) do
-    k = :binary.decode_unsigned(<<b1, b2, b3, b4>>, :big)
-    k = k * @c1
-    k = bor( ( k <<< @r1 ), ( k >>> ( 32 - @r1 ) ) )
-    k = k * @c2
-    hash = bxor( hash, k )
-    hash = bor( ( hash <<< @r2 ), ( hash >>> ( 32 - @r2 ) ) ) * @m + @n
+  def _mult_32( m, n ), do: ( m * n ) &&& @mask_32
+
+  def _rotl_32( num, off ), do: (( num <<< off ) ||| ( num >>> ( 32 - off ))) &&& @mask_32
+
+  def _bxor_with_self_rshift( val, rshift ), do: bxor( val, val >>> rshift )
+
+  def _proc_block( hash, << b1, b2, b3, b4, tail :: binary >> ) do
+    k1 =  ( b1 )        |||
+          ( b2 <<< 8 )  |||
+          ( b3 <<< 16 ) |||
+          ( b4 <<< 24 )
+    hash = hash
+      |> bxor( _mmix_32( k1 ) )
+      |> _rotl_32( 13 )
+    hash = ( hash * 5 + 0xe6546b64 ) &&& @mask_32
     _proc_block(hash, tail)
   end
 
-  def _proc_block(hash, << tail :: binary >>), do: { hash, tail }
+  def _proc_block( hash, << tail :: binary >> ), do: { hash, tail }
 
-  def _proc_tail(hash, << b1, b2, b3 >>, k1) do
-    k1 = bxor( k1, b3 <<< 16 )
-    _proc_tail(hash, << b1, b2 >>, k1)
+  def _mmix_32( k1 ) do
+    k1
+      |> _mult_32( 0xcc9e2d51 )
+      |> _rotl_32( 15 )
+      |> _mult_32( 0x1b873593 )
   end
 
-  def _proc_tail(hash, << b1, b2 >>, k1) do
-    k1 = bxor( k1, b2 <<< 8 )
-    _proc_tail(hash, << b1 >>, k1)
+  def _fmix_32( hash ) do
+    hash
+      |> band( @mask_32 )
+      |> _bxor_with_self_rshift( 16 )
+      |> _mult_32( 0x85ebca6b )
+      |> _bxor_with_self_rshift( 13 )
+      |> _mult_32( 0xc2b2ae35 )
+      |> _bxor_with_self_rshift( 16 )
   end
 
-  def _proc_tail(hash, << b1 >>, k1) do
-    k1 = bxor( k1, b1 )
-    k1 = k1 * @c1
-    k1 = bor( k1 <<< @r1, k1 >>> (32 - @r1) )
-    k1 = k1 * @c2
-    bxor( hash, k1 )
+  def _proc_tail( hash, << b0, b1, b2 >>, k1 ) do
+    k1 = ( k1 <<< 8 ) ||| b2
+    _proc_tail( hash, << b0, b1 >>, k1 )
   end
 
-  def _bxor_self_with_rshift(val, rshift), do: bxor( val, val >>> rshift )
+  def _proc_tail( hash, << b0, b1 >>, k1 ) do
+    k1 = ( k1 <<< 8 ) ||| b1
+    _proc_tail( hash, << b0 >>, k1 )
+  end
 
-  def _mult_by(val, mult), do: val * mult
+  def _proc_tail( hash, << b0 >>, k1 ) do
+    k1 = ( k1 <<< 8 ) ||| b0
+    bxor( hash, _mmix_32( k1 ) )
+  end
 
-  def murmur3_32(key, seed \\ 0x0000) do
+  def _proc_tail( hash, "", _ ), do: hash
+
+  def hash( key, seed \\ 0 ) do
     hash = seed
-    len = byte_size key
-    { hash, tail } = _proc_block(hash, key)
-    _proc_tail(hash, tail, 0x0000)
-      |> bxor len
-      |> _bxor_self_with_rshift 16
-      |> _mult_by 0x85ebca6b
-      |> _bxor_self_with_rshift 13
-      |> _mult_by 0xc2b2ae35
-      |> _bxor_self_with_rshift 16
+    len = byte_size( key )
+    { hash, tail } = _proc_block( hash, key )
+    hash = _proc_tail(hash, tail, 0)
+    hash
+      |> bxor( len )
+      |> _fmix_32
   end
 
 end
