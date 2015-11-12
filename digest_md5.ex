e02fd5be0eef9e4a5d15a31765ef34a3f79d61f6
4pcbr <me@4pcbr.com>
2015-11-12 22:37:14 +0100

    Implemented MD5

--- digest_md5.ex
+++ digest_md5.ex
@@ -1,30 +1,10 @@
+# Inspired by https://ostermiller.org/utils/src/MD5.java.html
+
 defmodule Digest.MD5 do
 
   use Bitwise
 
   @mask_32 0xFFFFFFFF
-
-  @s { 7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,  7, 12, 17, 22,
-       5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,  5,  9, 14, 20,
-       4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,  4, 11, 16, 23,
-       6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21,  6, 10, 15, 21 }
-
-  @k { 0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
-       0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-       0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
-       0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-       0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
-       0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-       0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
-       0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-       0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
-       0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-       0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
-       0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-       0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
-       0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-       0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
-       0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391 }
 
   @a0 0x67452301
   @b0 0xefcdab89
@@ -32,7 +12,6 @@ defmodule Digest.MD5 do
   @d0 0x10325476
 
 
-  defp _bin_of_length( n ), do: _bin_of_length( n, 0 )
   defp _bin_of_length( 0, _element ), do: <<>>
   defp _bin_of_length( n, element ) do
     << element >> <> _bin_of_length( n - 1, element )
@@ -55,16 +34,8 @@ defmodule Digest.MD5 do
       val when val <  56 -> 56 - left_over
       val when val >= 56 -> 120 - left_over
     end
-    IO.puts pad_len
     message = message <> << 0x80 >> <> _bin_of_length( pad_len - 1, 0 )
-    message = message <> _bump_bin_size( :binary.encode_unsigned( msg_len, :little ), 8 )
-    message |> _report
-  end
-
-
-  defp _report( val ) do
-    IO.inspect( val, limit: :infinity )
-    val
+    message <> _bump_bin_size( :binary.encode_unsigned( msg_len, :little ), 8 )
   end
 
 
@@ -75,48 +46,129 @@ defmodule Digest.MD5 do
   end
 
 
-  defp _md5( _a, b, c, d, ix ) when ix >= 0 and ix <= 15 do
-    {
-      ( ( b &&& c ) ||| ( bnot( b ) &&& d ) ) &&& @mask_32,
-      ix
-    }
-  end
-  defp _md5( _a, b, c, d, ix ) when ix >= 16 and ix <= 31 do
-    {
-      ( ( d &&& b ) ||| ( bnot( d ) &&& c ) ) &&& @mask_32,
-      rem( 5 * ix + 1, 16 )
-    }
-  end
-  defp _md5( _a, b, c, d, ix ) when ix >= 32 and ix <= 47 do
-    {
-      ( bxor( b, bxor( c, d ) ) ) &&& @mask_32,
-      rem( 3 * ix + 5, 16 )
-    }
-  end
-  defp _md5( _a, b, c, d, ix ) when ix >= 48 and ix <= 63 do
-    {
-      ( bxor( c, b ||| bnot( d ) ) ) &&& @mask_32,
-      rem( 7 * ix, 16 )
-    }
+  defp _FF( a, b, c, d, x, s, ac ) do
+    a = a + (( b &&& c ) ||| ( bnot( b ) &&& d ))
+    a = a + x
+    a = a + ac
+    a = _rotl_32( a, s )
+    ( a + b ) &&& @mask_32
   end
 
 
-  defp _chunk_loop( _, a, b, c, d, -1 ), do: { a, b, c, d }
-  defp _chunk_loop( words, a, b, c, d, ix ) do
-    { f, g } = _md5( a, b, c, d, ix )
-    { d, c, b, a } = {
-      c,
-      b,
-      (b + _rotl_32( (a + f + elem( @k, ix ) +
-        :binary.decode_unsigned(
-          Enum.at( words, g ),
-          :little
-        )) &&& @mask_32,
-        elem( @s, ix )
-      )) &&& @mask_32, 
-      d
-    }
-    _chunk_loop( words, a, b, c, d, ix - 1 )
+  defp _GG( a, b, c, d, x, s, ac ) do
+    a = a + (( b &&& d ) ||| ( c &&& bnot( d ) ))
+    a = a + x
+    a = a + ac
+    a = _rotl_32( a, s )
+    ( a + b ) &&& @mask_32
+  end
+
+
+  defp _HH( a, b, c, d, x, s, ac ) do
+    a = a + bxor( bxor( b, c ), d )
+    a = a + x
+    a = a + ac
+    a = _rotl_32( a, s )
+    ( a + b ) &&& @mask_32
+  end
+
+
+  defp _II( a, b, c, d, x, s, ac ) do
+    a = a + bxor( c, b ||| bnot( d ) )
+    a = a + x
+    a = a + ac
+    a = _rotl_32( a, s )
+    ( a + b ) &&& @mask_32
+  end
+
+
+  defp _round1({ a, b, c, d, words }) do
+    a = _FF( a, b, c, d, Enum.at( words,  0 ),   7, 0xd76aa478 )
+    d = _FF( d, a, b, c, Enum.at( words,  1 ),  12, 0xe8c7b756 )
+    c = _FF( c, d, a, b, Enum.at( words,  2 ),  17, 0x242070db )
+    b = _FF( b, c, d, a, Enum.at( words,  3 ),  22, 0xc1bdceee )
+    a = _FF( a, b, c, d, Enum.at( words,  4 ),   7, 0xf57c0faf )
+    d = _FF( d, a, b, c, Enum.at( words,  5 ),  12, 0x4787c62a )
+    c = _FF( c, d, a, b, Enum.at( words,  6 ),  17, 0xa8304613 )
+    b = _FF( b, c, d, a, Enum.at( words,  7 ),  22, 0xfd469501 )
+    a = _FF( a, b, c, d, Enum.at( words,  8 ),   7, 0x698098d8 )
+    d = _FF( d, a, b, c, Enum.at( words,  9 ),  12, 0x8b44f7af )
+    c = _FF( c, d, a, b, Enum.at( words, 10 ),  17, 0xffff5bb1 )
+    b = _FF( b, c, d, a, Enum.at( words, 11 ),  22, 0x895cd7be )
+    a = _FF( a, b, c, d, Enum.at( words, 12 ),   7, 0x6b901122 )
+    d = _FF( d, a, b, c, Enum.at( words, 13 ),  12, 0xfd987193 )
+    c = _FF( c, d, a, b, Enum.at( words, 14 ),  17, 0xa679438e )
+    b = _FF( b, c, d, a, Enum.at( words, 15 ),  22, 0x49b40821 )
+    { a, b, c, d, words }
+  end
+
+  defp _round2({ a, b, c, d, words }) do
+    a = _GG( a, b, c, d, Enum.at( words,  1 ),   5, 0xf61e2562 )
+    d = _GG( d, a, b, c, Enum.at( words,  6 ),   9, 0xc040b340 )
+    c = _GG( c, d, a, b, Enum.at( words, 11 ),  14, 0x265e5a51 )
+    b = _GG( b, c, d, a, Enum.at( words,  0 ),  20, 0xe9b6c7aa )
+    a = _GG( a, b, c, d, Enum.at( words,  5 ),   5, 0xd62f105d )
+    d = _GG( d, a, b, c, Enum.at( words, 10 ),   9, 0x02441453 )
+    c = _GG( c, d, a, b, Enum.at( words, 15 ),  14, 0xd8a1e681 )
+    b = _GG( b, c, d, a, Enum.at( words,  4 ),  20, 0xe7d3fbc8 )
+    a = _GG( a, b, c, d, Enum.at( words,  9 ),   5, 0x21e1cde6 )
+    d = _GG( d, a, b, c, Enum.at( words, 14 ),   9, 0xc33707d6 )
+    c = _GG( c, d, a, b, Enum.at( words,  3 ),  14, 0xf4d50d87 )
+    b = _GG( b, c, d, a, Enum.at( words,  8 ),  20, 0x455a14ed )
+    a = _GG( a, b, c, d, Enum.at( words, 13 ),   5, 0xa9e3e905 )
+    d = _GG( d, a, b, c, Enum.at( words,  2 ),   9, 0xfcefa3f8 )
+    c = _GG( c, d, a, b, Enum.at( words,  7 ),  14, 0x676f02d9 )
+    b = _GG( b, c, d, a, Enum.at( words, 12 ),  20, 0x8d2a4c8a )
+    { a, b, c, d, words }
+  end
+
+  defp _round3({ a, b, c, d, words }) do
+    a = _HH( a, b, c, d, Enum.at( words,  5 ),   4, 0xfffa3942 )
+    d = _HH( d, a, b, c, Enum.at( words,  8 ),  11, 0x8771f681 )
+    c = _HH( c, d, a, b, Enum.at( words, 11 ),  16, 0x6d9d6122 )
+    b = _HH( b, c, d, a, Enum.at( words, 14 ),  23, 0xfde5380c )
+    a = _HH( a, b, c, d, Enum.at( words,  1 ),   4, 0xa4beea44 )
+    d = _HH( d, a, b, c, Enum.at( words,  4 ),  11, 0x4bdecfa9 )
+    c = _HH( c, d, a, b, Enum.at( words,  7 ),  16, 0xf6bb4b60 )
+    b = _HH( b, c, d, a, Enum.at( words, 10 ),  23, 0xbebfbc70 )
+    a = _HH( a, b, c, d, Enum.at( words, 13 ),   4, 0x289b7ec6 )
+    d = _HH( d, a, b, c, Enum.at( words,  0 ),  11, 0xeaa127fa )
+    c = _HH( c, d, a, b, Enum.at( words,  3 ),  16, 0xd4ef3085 )
+    b = _HH( b, c, d, a, Enum.at( words,  6 ),  23, 0x04881d05 )
+    a = _HH( a, b, c, d, Enum.at( words,  9 ),   4, 0xd9d4d039 )
+    d = _HH( d, a, b, c, Enum.at( words, 12 ),  11, 0xe6db99e5 )
+    c = _HH( c, d, a, b, Enum.at( words, 15 ),  16, 0x1fa27cf8 )
+    b = _HH( b, c, d, a, Enum.at( words,  2 ),  23, 0xc4ac5665 )
+    { a, b, c, d, words }
+  end
+  
+  defp _round4({ a, b, c, d, words }) do
+    a = _II( a, b, c, d, Enum.at( words,  0 ),   6, 0xf4292244 )
+    d = _II( d, a, b, c, Enum.at( words,  7 ),  10, 0x432aff97 )
+    c = _II( c, d, a, b, Enum.at( words, 14 ),  15, 0xab9423a7 )
+    b = _II( b, c, d, a, Enum.at( words,  5 ),  21, 0xfc93a039 )
+    a = _II( a, b, c, d, Enum.at( words, 12 ),   6, 0x655b59c3 )
+    d = _II( d, a, b, c, Enum.at( words,  3 ),  10, 0x8f0ccc92 )
+    c = _II( c, d, a, b, Enum.at( words, 10 ),  15, 0xffeff47d )
+    b = _II( b, c, d, a, Enum.at( words,  1 ),  21, 0x85845dd1 )
+    a = _II( a, b, c, d, Enum.at( words,  8 ),   6, 0x6fa87e4f )
+    d = _II( d, a, b, c, Enum.at( words, 15 ),  10, 0xfe2ce6e0 )
+    c = _II( c, d, a, b, Enum.at( words,  6 ),  15, 0xa3014314 )
+    b = _II( b, c, d, a, Enum.at( words, 13 ),  21, 0x4e0811a1 )
+    a = _II( a, b, c, d, Enum.at( words,  4 ),   6, 0xf7537e82 )
+    d = _II( d, a, b, c, Enum.at( words, 11 ),  10, 0xbd3af235 )
+    c = _II( c, d, a, b, Enum.at( words,  2 ),  15, 0x2ad7d2bb )
+    b = _II( b, c, d, a, Enum.at( words,  9 ),  21, 0xeb86d391 )
+    { a, b, c, d, words }
+  end
+
+
+  defp _process_chunk( words, a, b, c, d ) do
+    { a, b, c, d, words }
+      |> _round1
+      |> _round2
+      |> _round3
+      |> _round4
   end
 
 
@@ -125,12 +177,11 @@ defmodule Digest.MD5 do
   end
   defp _main_loop( message, offset, len, a, b, c, d ) do
     { a0, b0, c0, d0 } = { a, b, c, d }
-    IO.inspect { a, b, c, d }
-    { a, b, c, d } = message
+    { a, b, c, d, _} = message
                       |> :binary.part( { offset, len } )
                       |> _chunk_bin( 4 )
-                      |> _report
-                      |> _chunk_loop( a, b, c, d, 64 - 1 )
+                      |> Enum.map( &(:binary.decode_unsigned( &1, :little )))
+                      |> _process_chunk( a, b, c, d )
     _main_loop(
       message,
       offset + len,
@@ -147,8 +198,6 @@ defmodule Digest.MD5 do
     { a, b, c, d } = message
                       |> _pad
                       |> _main_loop( 0, 64, @a0, @b0, @c0, @d0 )
-    IO.puts "a: #{a}, b: #{b}, c: #{c}, d: #{d}"
-    IO.inspect :binary.encode_unsigned( a, :little )
 
     :binary.encode_unsigned( a, :little ) <>
       :binary.encode_unsigned( b, :little ) <>
@@ -156,19 +205,4 @@ defmodule Digest.MD5 do
           :binary.encode_unsigned( d, :little )
   end
 
-  def test( msg ) do
-    core = :erlang.md5( msg )
-    mine = Digest.MD5.hash( msg )
-    IO.inspect core
-    IO.inspect mine
-    if core == mine do
-      { :ok }
-    else
-      { :not_ok }
-    end
-  end
-
-
 end
-
-IO.inspect Digest.MD5.test("a")
