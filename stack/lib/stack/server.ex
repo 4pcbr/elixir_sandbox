5555528e079d648e6158d59cb670408ab3ccda51
4pcbr <me@4pcbr.com>
2015-11-24 10:11:20 +0100

    Added the sub-supervisor and stash-server in the stack application

--- stack/lib/stack/server.ex
+++ stack/lib/stack/server.ex
@@ -2,8 +2,8 @@ defmodule Stack.Server do
 
   use GenServer
 
-  def start_link(stack) do
-    GenServer.start_link(__MODULE__, stack, name: __MODULE__)
+  def start_link( stash_pid ) do
+    { :ok, _pid } = GenServer.start_link(__MODULE__, stash_pid, name: __MODULE__)
   end
 
   def pop do
@@ -19,23 +19,26 @@ defmodule Stack.Server do
   end
 
 
-  def handle_call(:pop, _from, stack) do
-    [ el | stack_tail ] = stack
-    { :reply, el, stack_tail }
+  def init( stash_pid ) do
+    current_stack = Stack.Stash.get_value stash_pid
+    { :ok, { current_stack, stash_pid } }
   end
 
-  def handle_cast({:push, el}, stack) do
-    { :noreply, [el] ++ stack }
+  def handle_call(:pop, _from, { current_stack, stash_pid }) do
+    [ el | stack_tail ] = current_stack
+    { :reply, el, { stack_tail, stash_pid } }
+  end
+
+  def handle_cast({:push, el}, { current_stack, stash_pid }) do
+    { :noreply, { [el] ++ current_stack, stash_pid } }
   end
 
   def format_status(_reason, [ _pdict, state ]) do
     [ data: [ { 'State', "My current state is '#{inspect state}', and I'm happy" } ] ]
   end
 
-  def terminate(reason, state) do
-    IO.inspect reason
-    IO.inspect state
-    { :stop, reason, "stop", state }
+  def terminate(reason, { current_stack, stash_pid }) do
+    Stack.Stash.save_value stash_pid, current_stack
   end
 
 end
---
+++ stack/lib/stack/stash.ex
@@ -0,0 +1,27 @@
+defmodule Stack.Stash do
+
+  use GenServer
+
+  def start_link( current_stack ) do
+    { :ok, pid } = GenServer.start_link( __MODULE__, current_stack )
+  end
+
+  def save_value( pid, value ) do
+    GenServer.cast pid, { :save_value, value }
+  end
+
+  def get_value( pid ) do
+    GenServer.call pid, :get_value
+  end
+
+
+  def handle_call( :get_value, _from, current_stack ) do
+    { :reply, current_stack, current_stack }
+  end
+
+  def handle_cast( { :save_value, stack }, _current_stack ) do
+    { :noreply, stack }
+  end
+
+end
+
