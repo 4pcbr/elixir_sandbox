7cc76f42058799bd5e1ee1f9cd4186eea59f1e56
4pcbr <me@4pcbr.com>
2015-11-28 12:08:50 +0100

    Implemented a dynamic function macro

--- myif.exs
+++ myif.exs
@@ -1,18 +1,55 @@
 defmodule My do
 
-  def myif( condition, clauses ) do
-
-    IO.inspect clauses
-
+  defmacro if( condition, clauses ) do
     do_clause = Keyword.get( clauses, :do, nil )
     else_clause = Keyword.get( clauses, :else, nil )
+    quote do
+      case unquote( condition ) do
+        val when val in [ false, nil ] -> unquote( else_clause )
+        _ -> unquote( do_clause )
+      end
+    end
+  end
 
-    case condition do
-      val when val in [ false, nil ]
-        -> else_clause
-      _otherwise
-        -> do_clause
+  defmacro unless( condition, clauses ) do
+    do_clause = Keyword.get( clauses, :do, nil )
+    else_clause = Keyword.get( clauses, :else, nil )
+    quote do
+      case unquote( condition ) do
+        val when val in [ false, nil ] -> unquote( do_clause )
+        _ -> unquote( else_clause )
+      end
+    end
+  end
+
+  defmacro times_n( n ) do
+    quote do
+      def unquote(:"times_#{n}")(k) do
+        unquote(n) * k
+      end
     end
   end
 
 end
+
+defmodule Test do
+
+  require My
+
+  My.if 1 == 2 do
+    IO.puts "1 == 2"
+  else
+    IO.puts "1 != 2"
+  end
+
+  My.unless 1 == 2 do
+    IO.puts "1 != 2"
+  else
+    IO.puts "1 == 2"
+  end
+
+  My.times_n(3)
+
+end
+
+IO.puts Test.times_3(5)
