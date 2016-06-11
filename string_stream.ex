80c57dfbc350b79a8702a08afa2152c48383e91e
4pcbr <me@4pcbr.com>
2016-06-12 00:31:48 +0200

    Limited StringStream public interface to count_words/2 and build_trie/1 + wrote some readme

--- string_stream.ex
+++ string_stream.ex
@@ -1,8 +1,35 @@
+# The problem: String stream
+#
+# Given a stream of bytes (read: one can read
+# one byte at a time, the size of the stream is
+# unlimited. Given the dictionary: a set of the
+# known words.
+# Count the number of the dictionary words in
+# the stream.
+# Example:
+#   disctionary: [ "cat", "car" ]
+#   stream string: "catcarcatcan"
+#   expected result: %{ "cat" -> 2, "car" => 1 }
+
 ExUnit.start()
 
 defmodule StringStream do
 
-  def update_index( trie, [] ) do
+  def count_words( stream, words ) do
+    trie = build_trie words
+    { _, _, counters } = look_ahead( stream, trie, [], 0, %{} )
+    counters
+  end
+
+  def build_trie( words ) do
+    words
+      |> Enum.map( &String.to_char_list(&1) )
+      |> Enum.reduce( %{}, fn word, trie ->
+        update_index( trie, word )
+      end )
+  end
+
+  defp update_index( trie, [] ) do
     # The end of the word is marked by a zero-byte
     # This allows us to store the end of the word and longer alternatives
     # Example:
@@ -14,22 +41,22 @@ defmodule StringStream do
     Map.put( trie, 0, true )
   end
 
-  def update_index( trie, [ char | tail ] ) do
+  defp update_index( trie, [ char | tail ] ) do
     { _, trie } = Map.get_and_update( trie, char, fn sub_trie ->
       sub_trie = case sub_trie do
         nil -> %{}
-        _ -> sub_trie
+        _   -> sub_trie
       end
       { sub_trie, update_index( sub_trie, tail ) }
     end )
     trie
   end
 
-  def increment( counters, word ) do
+  defp increment( counters, word ) do
     { _, counters } = Map.get_and_update( counters, word, fn cur_value ->
       case cur_value do
         nil -> { cur_value, 1 }
-        v -> { cur_value, v + 1 }
+        v   -> { cur_value, v + 1 }
       end
     end )
     counters
@@ -37,23 +64,23 @@ defmodule StringStream do
 
   # A corner case: when the stream is over and we've found
   # a dictionary word
-  def look_ahead( stream = [], %{ 0 => true }, word, _, counters ) do
+  defp look_ahead( stream = [], %{ 0 => true }, word, _, counters ) do
     { stream, word, counters }
   end
 
   # The stream is over
-  def look_ahead( [], _, _, _, counters ) do
+  defp look_ahead( [], _, _, _, counters ) do
     { [], nil, counters }
   end
 
   # No subtree === no matching string found
-  def look_ahead( stream, nil, _buf, _trie_ix, counters ) do
+  defp look_ahead( stream, nil, _buf, _trie_ix, counters ) do
     { stream, nil, counters }
   end
 
   # A case when there is a null-byte in the trie which means
   # we found the end of the word
-  def look_ahead( stream = [ ch | tail ], sub_trie = %{ 0 => true }, buf, trie_ix, counters ) do
+  defp look_ahead( stream = [ ch | tail ], sub_trie = %{ 0 => true }, buf, trie_ix, counters ) do
     case Map.keys( sub_trie ) |> Enum.count do
       # The word is the longest possible in this branch 
       # In a non-greedy algorithm this branch should be removed
@@ -72,7 +99,7 @@ defmodule StringStream do
   end
 
   # The entrypoint: we're at the top of the trie
-  def look_ahead( [ ch | tail ], trie, buf, trie_ix = 0, counters ) do
+  defp look_ahead( [ ch | tail ], trie, buf, trie_ix = 0, counters ) do
     case look_ahead( tail, Map.get( trie, ch ), buf ++ [ ch ], trie_ix + 1, counters ) do
       { _, nil, _counters } ->
         look_ahead( tail, trie, [], trie_ix, counters )
@@ -83,7 +110,7 @@ defmodule StringStream do
     end
   end
 
-  def look_ahead( [ ch | tail ], trie, buf, trie_ix, counters ) do
+  defp look_ahead( [ ch | tail ], trie, buf, trie_ix, counters ) do
     # Routinely read a char from the stream, append it to the buffer
     # and increment the trie index
     look_ahead( tail, Map.get( trie, ch ), buf ++ [ ch ], trie_ix + 1, counters )
@@ -96,88 +123,51 @@ defmodule StringStreamTest do
   use ExUnit.Case
   import StringStream
 
-  def build_trie( words ) do
-    words
-      |> Enum.map( &String.to_char_list(&1) )
-      |> Enum.reduce( %{}, fn word, trie ->
-        update_index( trie, word )
-      end )
-  end
-
   test "One word test-case" do
-    trie = build_trie ~w(cat)
-    test_string = 'cat'
-    { _, _, counters } = look_ahead( test_string, trie, [], 0, %{} )
-    assert counters == %{ 'cat' => 1 }
+    assert count_words( 'cat', ~w(cat) ) == %{ 'cat' => 1 }
   end
 
   test "One word occures multiple times" do
-    trie = build_trie ~w(cat)
-    test_string = 'catcatcat'
-    { _, _, counters } = look_ahead( test_string, trie, [], 0, %{} )
-    assert counters == %{ 'cat' => 3 }
+    assert count_words( 'catcatcat', ~w(cat) ) == %{ 'cat' => 3 }
   end
 
   test "lookup for some basic words" do
-    trie = build_trie ~w(cat car can)
-    test_string = 'catcancancatcar'
-    { _, _, counters } = look_ahead( test_string, trie, [], 0, %{} )
-    assert counters == %{ 'cat' => 2, 'can' => 2, 'car' => 1 }
+    assert count_words( 'catcancancatcar', ~w(cat car can) ) == %{ 'cat' => 2, 'can' => 2, 'car' => 1 }
   end
 
   test "Some garbage symbols in the front" do
-    trie = build_trie ~w(cat car can)
-    test_string = 'asdasdcatcancar'
-    { _, _, counters } = look_ahead( test_string, trie, [], 0, %{} )
-    assert counters == %{ 'cat' => 1, 'can' => 1, 'car' => 1 }
+    assert count_words( 'asdasdcatcancar', ~w(cat car can)) == %{ 'cat' => 1, 'can' => 1, 'car' => 1 }
   end
 
   test "Some garbage symbols in the tail" do
-    trie = build_trie ~w(cat can car)
-    test_string = 'cancatcarasdasd'
-    { _, _, counters } = look_ahead( test_string, trie, [], 0, %{} )
-    assert counters == %{ 'can' => 1, 'cat' => 1, 'car' => 1 }
+    assert count_words( 'cancatcarasdasd', ~w(cat can car) ) == %{ 'can' => 1, 'cat' => 1, 'car' => 1 }
   end
 
   test "Some garbage symbols in the middle" do
-    trie = build_trie ~W(cat can car)
-    test_string = 'canasdcatasdcar'
-    { _, _, counters } = look_ahead( test_string, trie, [], 0, %{} )
-    assert counters == %{ 'can' => 1, 'cat' => 1, 'car' => 1 }
+    assert count_words( 'canasdcatasdcar', ~w(cat can car) ) == %{ 'can' => 1, 'cat' => 1, 'car' => 1 }
   end
 
   test "Greedy search" do
-    trie = build_trie ~w(alpha alphabet)
-    test_string = 'alphabet'
-    { _, _, counters } = look_ahead( test_string, trie, [], 0, %{} )
-    assert counters == %{ 'alphabet' => 1 }
+    assert count_words( 'alphabet', ~w(alpha alphabet)) == %{ 'alphabet' => 1 }
   end
 
   test "Greedy search with both words in the test string" do
-    trie = build_trie ~w(alpha alphabet)
-    test_string = 'alphabetalphabe'
-    { _, _, counters } = look_ahead( test_string, trie, [], 0, %{} )
-    assert counters == %{ 'alphabet' => 1, 'alpha' => 1 }
+    assert count_words( 'alphabetalphabe', ~w(alpha alphabet) ) == %{ 'alphabet' => 1, 'alpha' => 1 }
   end
 
   test "Building a trie" do
-    assert %{}
-      |> update_index( 'cat'      )
-      |> update_index( 'car'      )
-      |> update_index( 'alpha'    )
-      |> update_index( 'alphabet' ) ==
-        %{
-          97 => %{
-            108 => %{
-              112 => %{
-                104 => %{
-                  97 => %{
-                    0 => true,
-                    98 => %{
-                      101 => %{
-                        116 => %{
-                          0 => true
-                        },
+    assert build_trie( ~w(cat car alpha alphabet) )
+      %{
+        97 => %{
+          108 => %{
+            112 => %{
+              104 => %{
+                97 => %{
+                  0 => true,
+                  98 => %{
+                    101 => %{
+                      116 => %{
+                        0 => true
                       },
                     },
                   },
@@ -185,18 +175,18 @@ defmodule StringStreamTest do
               },
             },
           },
-          99 => %{
-            97 => %{
-              116 => %{
-                0 => true
-              },
-              114 => %{
-                0 => true
-              },
+        },
+        99 => %{
+          97 => %{
+            116 => %{
+              0 => true
+            },
+            114 => %{
+              0 => true
             },
           },
-        }
+        },
+      }
   end
 
 end
-
