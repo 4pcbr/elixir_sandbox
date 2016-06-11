e49cbee07a8ca0a7b6384765645af95addb800fd
4pcbr <me@4pcbr.com>
2016-06-11 23:59:57 +0200

    Some minor fixes in string_stream: more comments and an example

--- string_stream.ex
+++ string_stream.ex
@@ -3,6 +3,14 @@ ExUnit.start()
 defmodule StringStream do
 
   def update_index( trie, [] ) do
+    # The end of the word is marked by a zero-byte
+    # This allows us to store the end of the word and longer alternatives
+    # Example:
+    #                                [\0] <- word "alpha"
+    #                               /
+    # [a] - [l] - [p] - [h] - [a] <
+    #                               \
+    #                                [b] - [e] - [t] <- word "alphabet"
     Map.put( trie, 0, true )
   end
 
@@ -27,47 +35,58 @@ defmodule StringStream do
     counters
   end
 
+  # A corner case: when the stream is over and we've found
+  # a dictionary word
   def look_ahead( stream = [], %{ 0 => true }, word, _, counters ) do
     { stream, word, counters }
   end
 
+  # The stream is over
   def look_ahead( [], _, _, _, counters ) do
     { [], nil, counters }
   end
 
+  # No subtree === no matching string found
   def look_ahead( stream, nil, _buf, _trie_ix, counters ) do
     { stream, nil, counters }
   end
 
+  # A case when there is a null-byte in the trie which means
+  # we found the end of the word
   def look_ahead( stream = [ ch | tail ], sub_trie = %{ 0 => true }, buf, trie_ix, counters ) do
     case Map.keys( sub_trie ) |> Enum.count do
+      # The word is the longest possible in this branch 
+      # In a non-greedy algorithm this branch should be removed
       1 ->
         { stream, buf, counters }
+      # We've found a word but it might appear to be not the longest
+      # matching substring, move on as it's a greedy algo implementation
       _ ->
-        # it's a greedy algorithm
         case look_ahead( tail, Map.get( sub_trie, ch ), buf ++ [ ch ], trie_ix + 1, counters ) do
+          # Ok, we did our best but a longer match wasn't found
           { _, nil, _counters } -> { stream, buf, counters }
+          # Alright, a longer variant found
           { new_stream, new_word, new_counters } -> { new_stream, new_word, new_counters }
         end
     end
   end
 
+  # The entrypoint: we're at the top of the trie
   def look_ahead( [ ch | tail ], trie, buf, trie_ix = 0, counters ) do
     case look_ahead( tail, Map.get( trie, ch ), buf ++ [ ch ], trie_ix + 1, counters ) do
       { _, nil, _counters } ->
         look_ahead( tail, trie, [], trie_ix, counters )
       { stream, word, counters } -> 
+        # Note: counters increment happening only on the top level
+        # of the recursion
         look_ahead( stream, trie, [], trie_ix, counters |> increment( word ) )
     end
   end
 
   def look_ahead( [ ch | tail ], trie, buf, trie_ix, counters ) do
+    # Routinely read a char from the stream, append it to the buffer
+    # and increment the trie index
     look_ahead( tail, Map.get( trie, ch ), buf ++ [ ch ], trie_ix + 1, counters )
-  end
-
-  def inspect_obj(o) do
-    IO.inspect o
-    o
   end
 
 end
